2d2afacd083bdeddadbf430a1f0a7768f0c680d1
Emrald-T <emraldthadathil@sbpcorp.in>
2020-05-23 18:00:00 +0530

    initial comming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PopoverComponent } from "./popover/popover.component";
+import Quill from 'quill';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'EditorApp';
+  quill: any;
+  // template: PopoverComponent
+
+  ngOnInit () {
+    var quill = new Quill('#editor-container', {
+      modules: {
+        toolbar: '#toolbar'
+      },
+      placeholder: 'Enter here...',
+      theme: 'snow'  // or 'bubble'
+    });
+    quill.focus();
+
+    var customButton = document.querySelector('#custom-button');
+    customButton.addEventListener('click', function() {
+      // var range = quill.getSelection();
+      // if (range) {
+      //   quill.insertText(range.index, "Ω");
+      // }
+      (<any>$('[data-toggle="popover"]')).attr("data-content",(
+        new PopoverComponent()
+      )).popover();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PopoverComponent } from './popover/popover.component';
+import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopoverComponent
   ],
   imports: [
     BrowserModule
